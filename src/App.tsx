676085a739aec68b712bf9502691531f0d10c3f8
Q-Zaid <zaid@qluster.co>
2021-06-27 21:55:14 +1000

    feat: implemented Pixel tracking

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import { Landing } from "./Landing"
 import { SignupSuccess } from "./SignupSuccess"
+import ReactGA from "react-ga"
+
+const TRACKING_ID = "" // Google Analytics ID
+ReactGA.initialize(TRACKING_ID)
 
 export const App = () =>
     <Router>
