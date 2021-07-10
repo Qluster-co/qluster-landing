2dc2990f1de30e9962afe13fcfa18fc08fc787d4
Q-Zaid <zaid@qluster.co>
2021-07-10 16:03:42 +1000

    feat: implemented GA tracking

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import { Landing } from "./Landing"
 import { SignupSuccess } from "./SignupSuccess"
-import ReactGA from "react-ga"
-
-const TRACKING_ID = "" // Google Analytics ID
-ReactGA.initialize(TRACKING_ID)
 
 export const App = () =>
     <Router>
